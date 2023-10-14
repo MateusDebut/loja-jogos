ccc3c981702e1c438bcc6f069bd2a9e1fa6567c3
MateusDebut <mateusdossantosribeiro100@usp.br>
2023-10-14 01:18:26 -0300

    ajuste

---
+++ jogos-front/src/app/components/views/cadastro/cadastro.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Jogo } from 'src/app/model/jogo';
+import { JogoService } from 'src/app/services/jogo.service';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent implements OnInit{
+  public jogo: Jogo = new Jogo();
+
+  constructor(private jogoService: JogoService) { }
+
+  ngOnInit() {
+
+  }
+
+  saveJogo(){
+    this.jogoService.postJogo(this.jogo).subscribe(resposta => {
+      alert("O jogo " + this.jogo.nome + "foi cadastrado com sucesso!");
+    });
+  }
+
+
+}
